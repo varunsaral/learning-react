a8a7816168f3177d3305607de0e21875f1d77b38
= <=>
2021-11-24 18:19:58 +0530

    empty task page

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -4,10 +4,11 @@ function Todos(props) {
     return (
         <div className="container">
             <h3 className="text-center my-3"> Todos List</h3>
-            
-            {props.todos.map((todo)=>{
+            {props.todos.length == 0 ?"No more task available":
+            props.todos.map((todo)=>{
                 return <Todo todo = {todo} key={todo.sno} ondelete = {props.ondelete}/>      
-            })}
+            })
+        }
            
         </div>
     )
