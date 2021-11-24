a399fc6288dfb2ad6476c44367f9f2770591c1c5
= <=>
2021-11-25 03:46:18 +0530

    lastofdat

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -6,18 +6,13 @@ function Todos(props) {
     }
     return (
        
-        <div className="container my-3"  style={mystyle}>
-            <h3 className="my-3"> Todos List</h3>
-            {props.todos.length === 0 ?"No more task available":
-            props.todos.map((todo)=>{
-               
-                return (
-                    <>
-                    <Todo todo = {todo} key={todo.sno} ondelete = {props.ondelete}/> <hr/>
-                    </> 
-                )  
-            })
-        }
+        <div className="container">
+        <h3 className="text-center my-3"> Todos List</h3>
+        {props.todos.length == 0 ?"No more task available":
+        props.todos.map((todo)=>{
+            return <Todo todo = {todo} key={todo.sno} ondelete = {props.ondelete}/>      
+        })
+    }
         </div>
     ) 
 }
